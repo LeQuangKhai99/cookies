05b72c52ba100d11bef6e348729e366c022b698c
khai lee <fcxomben99@gmail.com>
2022-05-03 21:57:04 +0700

    fix error

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('login', function() {
+    return 'Login page';
 });
+
+Route::get('buy/{cookies}', function ($cookies) {
+    if(!is_numeric($cookies)) {
+        return 'Error, param is not a number';
+    }
+
+    $user = Auth::user();
+    $wallet = $user->wallet;
+
+    if($wallet < $cookies) {
+        return "You don't have enough money";
+    }
+
+    $user->update(['wallet' =>$wallet - $cookies * 1]);
+    Log:info('User ' . $user->email . ' have bought ' . $cookies . ' cookies'); // we need to log who ordered and how much
+    return 'Success, you have bought ' . $cookies . ' cookies!';
+})->middleware('check-auth');
